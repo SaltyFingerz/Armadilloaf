4804261d9b681a9395b1b70d9828832bfadf940f
BensariB <1903102@uad.ac.uk>
2023-03-06 14:59:51 +0000

    Revert "player retaining velocity"
    This reverts commit 1667d0ab5287fc770b0ec96565bb15d7306edcef.

--- Assets/Scripts/PlayerLaunchScript.cs
+++ Assets/Scripts/PlayerLaunchScript.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 using UnityEngine.Playables;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class PlayerLaunchScript : MonoBehaviour
 {
@@ -16,7 +15,6 @@ public class PlayerLaunchScript : MonoBehaviour
     public CinemachineFreeLook M_holdDownCamera;
     public CinemachineFreeLook M_freeRotationCamera;
     public GameObject M_playerManager;
-    public Transform M_groundPoint;
     enum CameraMode { holdDown, freeRotation };
     CameraMode m_cameraMode;
 
@@ -38,8 +36,6 @@ public class PlayerLaunchScript : MonoBehaviour
     const float m_baseLength = 10.0f;
     public float M_angleChangeRadians;
     public float M_floorAngleChangeRadians;
-
-    public LayerMask M_whatIsGround;
 
 
     public void Start()
@@ -148,21 +144,14 @@ public class PlayerLaunchScript : MonoBehaviour
 
     void HandleLaunchInput()
     {
-        // Don't allow launching if the player is not on the ground
-        if (!m_isOnFloor)
-        {
-            return;
-        }
-
-        // Launch when input detected
+        //override player rotation
         if (Input.GetMouseButtonDown(0) || Input.GetKeyUp(KeyCode.Space))
         {
+            m_launchingDirection = m_direction;
+            m_launchingDirection.Normalize();
+            m_launchingStage++;
             LaunchingStart();
-            return;
         }
-
-        m_rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
-
         m_launchingPower += (m_currentScroll - Input.mouseScrollDelta.y);
 
         // clamp the power of the lauch between 0 and the limit
@@ -182,6 +171,7 @@ public class PlayerLaunchScript : MonoBehaviour
         m_rigidbody.velocity = Vector3.zero;
         m_rigidbody.angularVelocity = Vector3.zero;
         m_rigidbody.freezeRotation = false;
+        m_rigidbody.constraints = RigidbodyConstraints.FreezePositionX | RigidbodyConstraints.FreezePositionZ;
         M_arrow.SetActive(true);
         M_arrowMaximum.SetActive(true);
         M_arrow.transform.localScale = new Vector3(5f, 5f, 5f);
@@ -196,10 +186,6 @@ public class PlayerLaunchScript : MonoBehaviour
     }
     private void LaunchingStart()
     {
-        m_launchingDirection = m_direction;
-        m_launchingDirection.Normalize();
-        m_launchingStage++;
-
         m_rigidbody.constraints = RigidbodyConstraints.None;
         m_rigidbody.freezeRotation = true;
         M_arrow.SetActive(false);
@@ -275,8 +261,7 @@ public class PlayerLaunchScript : MonoBehaviour
   
     void OnCollisionStay(Collision a_collider)
     {
-        // check if the collision is with the layer of ground
-        if (M_whatIsGround == (M_whatIsGround | (1 << a_collider.gameObject.layer)))
+        if (a_collider.gameObject.tag == "Floor")
         {
             m_isOnFloor = true;
         }
@@ -287,8 +272,7 @@ public class PlayerLaunchScript : MonoBehaviour
     }
     private void OnCollisionExit(Collision a_collider)
     {
-        // check if the collision is with the layer of ground
-        if (M_whatIsGround == (M_whatIsGround | (1 << a_collider.gameObject.layer)))
+        if (a_collider.gameObject.tag == "Floor")
         {
             m_isOnFloor = false;
         }
