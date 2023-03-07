4c2ed8c029315a46cb31342fb32f386a60da987b
SaltyFingerz <denis.iannoukos@yandex.com>
2023-03-07 14:59:02 +0000

    Goal UI, shadow bug fixed, borders on isle

---
+++ Assets/Scripts/LevelProgressScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelProgressScript : MonoBehaviour
+{
+    public GameObject M_FinishUI;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Finish"))
+        {
+            M_FinishUI.SetActive(true);
+        }
+    }
+}
