280fc6422ec6f1cb25e5966493a79fb9da36a732
SaltyFingerz <denis.iannoukos@yandex.com>
2023-04-17 12:53:16 +0100

    can, cereal, pipe and bread audio collision implemented

--- Assets/CanAudioScript.cs
+++ Assets/CanAudioScript.cs
@@ -6,12 +6,21 @@ public class CanAudioScript : MonoBehaviour
 {
     [SerializeField] AudioClip[] CanCollided;
     AudioSource CanClash;
+    private bool m_canClash;
     // Start is called before the first frame update
     void Start()
     {
         CanClash = GetComponent<AudioSource>();
+        StartCoroutine(waitForCans());
     }
 
+    IEnumerator waitForCans()
+    {
+        yield return new WaitForSeconds(3);
+        m_canClash = true;
+
+    }
+  
     public void CanClashSound() //random sound of the player character taunting her opponents by exclaiming "Pathetic!" called upon clearing the enemies of a room.
     {
         AudioClip clip = CanCollided[UnityEngine.Random.Range(0, CanCollided.Length)];
@@ -21,7 +30,10 @@ public class CanAudioScript : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        CanClashSound();
+        if (m_canClash)
+        {
+            CanClashSound();
+        }
     }
 
    
---
+++ Assets/PlayerCollisionSFX.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollisionSFX : MonoBehaviour
+{
+    [SerializeField] AudioClip[] PCollided;
+    public AudioSource PHit;
+    private bool m_canHit = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+       
+    }
+
+    private void Update()
+    {
+        if (!m_canHit)
+        {
+            StartCoroutine(waitForP());
+        }
+    }
+
+    IEnumerator waitForP()
+    {
+        yield return new WaitForSeconds(1);
+        m_canHit = true;
+
+    }
+
+    public void PHitSound() //random sound of the player character taunting her opponents by exclaiming "Pathetic!" called upon clearing the enemies of a room.
+    {
+        AudioClip clip = PCollided[UnityEngine.Random.Range(0, PCollided.Length)];
+        PHit.PlayOneShot(clip);
+        m_canHit = false;
+        StartCoroutine(waitForP());
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (m_canHit)
+        {
+            PHitSound();
+        }
+    }
+}
