a8f3d2d9c7221ebd0e7dc70510623e60561bdb75
BensariB <1903102@uad.ac.uk>
2023-04-26 13:18:51 +0100

    rolling bug fixes

--- Assets/Scripts/PlayerManagerScript.cs
+++ Assets/Scripts/PlayerManagerScript.cs
@@ -195,7 +195,7 @@ public partial class PlayerManagerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        print("fluffed" + M_Fluffed);
+        //print("fluffed" + M_Fluffed);
        
 
         M_FruitUI.text = M_FruitCollected.ToString();
@@ -540,7 +540,7 @@ public partial class PlayerManagerScript : MonoBehaviour
     {
         
 
-       M_additionalCamera.SetActive(false);
+        M_additionalCamera.SetActive(false);
         M_walkingPlayer.SetActive(false);
         M_launchCamera.SetActive(true);
 
