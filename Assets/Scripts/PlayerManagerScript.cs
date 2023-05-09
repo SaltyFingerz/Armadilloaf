6f8e29e2cb833299fd2d165d89f8f3ec68c43302
BensariB <1903102@uad.ac.uk>
2023-05-09 11:41:31 +0100

    Revert "revert change"
    This reverts commit 1c0329685b2cb75037568944f8ddbfd403e02abe.

--- Assets/Scripts/PlayerManagerScript.cs
+++ Assets/Scripts/PlayerManagerScript.cs
@@ -1,11 +1,7 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using TMPro;
-using static PlayerManagerScript;
-using System;
 
 public partial class PlayerManagerScript : MonoBehaviour
 {
